63e4a5baa5c8440ebdf44e803a74f4ca3c81b6c0
rizqisamawa <48850296+rizqisamawa@users.noreply.github.com>
2022-05-16 12:18:34 +0700

    finish development

--- pages/about.js
+++ pages/about.js
@@ -8,19 +8,12 @@ function About() {
         <title>About | Muhammad Rizqi Samawa</title>
       </Head>
       <Navbar />
-
-      <div className="mx-24 items-center justify-center mt-20 text-center">
-        <p className="text-6xl font-sans font-semibold">Hello Everyone</p>
-        <p className="text-2xl font-mono mt-4 p-1">
-          {"My Name is Muhammad Rizqi Samawa, I'm a Fresh Graduate"}
-        </p>
-        <p className="text-xl font-sans p-1 font-light italic ">
-          I hope I become a useful person for others
-        </p>
-        <div className="text-center mt-10">
-          <button className="bg-gray-500 font-mono p-2 text-white rounded-lg hover:bg-gray-800">
-            npx rizqisamawa
-          </button>
+      <div className="mx-24 mt-20 items-center justify-center">
+        <p className="text-center font-sans text-2xl font-semibold md:text-6xl">Hello Everyone</p>
+        <p className="mt-4 p-1 text-justify font-mono md:text-center md:text-2xl">{"My Name is Muhammad Rizqi Samawa, I'm a Fresh Graduate"}</p>
+        <p className="p-1 text-justify font-sans text-sm font-light italic md:text-center md:text-xl">I hope I become a useful person for others</p>
+        <div className="mt-5 text-center md:mt-10">
+          <button className="rounded-lg bg-gray-700 p-2 font-mono text-white hover:bg-gray-900">npx rizqisamawa</button>
         </div>
       </div>
     </>
