f7a525fbcb49e10a548527291ce66d1529bd7966
rizqisamawa <48850296+rizqisamawa@users.noreply.github.com>
2022-05-16 15:24:23 +0700

    add responsive

--- components/navbar.js
+++ components/navbar.js
@@ -11,40 +11,36 @@ function Navbar({ router }) {
   ];
 
   return (
-    <nav className="">
-      <div className="py-8 flex justify-between items-center content-center mx-24">
-        <div>
-          <Link href="/">
-            <a className="text-2xl font-sans font-bold tracking-[5px]">
-              RIZQI SAMAWA
-            </a>
-          </Link>
-        </div>
-        <div>
-          <ul className="flex w-full space-x-8 justify-end font-mono text-xl tracking-wide">
-            {items.map((item) => {
-              return (
-                <div key={item.id}>
-                  <li>
-                    <Link href={item.href}>
-                      <a
-                        className={`${
-                          router.pathname == item.href
-                            ? "border-b-2 border-black"
-                            : "hover:border-b-2 border-black"
-                        }`}
-                      >
-                        {item.text}
-                      </a>
-                    </Link>
-                  </li>
-                </div>
-              );
-            })}
-          </ul>
-        </div>
+     <div className="justify-between md:flex items-center mx-22">
+      <div className="text-center font-sans text-2xl font-bold tracking-[5px] p-4 ml-3">
+        <Link href="/">
+          <a>RIZQI SAMAWA</a>
+        </Link>
       </div>
-    </nav>
+      <div>
+        <ul className="flex justify-center space-x-5 p-2 text-xl font-mono tracking-wide mr-4">
+          {items.map((item) => {
+            return (
+              <div key={item.id}>
+                <li>
+                  <Link href={item.href}>
+                    <a
+                      className={`${
+                        router.pathname == item.href
+                          ? "border-b-2 border-black"
+                          : "hover:border-b-2 border-black"
+                      }`}
+                    >
+                      {item.text}
+                    </a>
+                  </Link>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
 }
 
