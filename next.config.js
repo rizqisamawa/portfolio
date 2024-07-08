e3ad4e67d92dd0b9c617003e39e0a868b4000803
rizqisamawa <rizqisamawa11@gmail.com>
2024-07-08 10:03:01 +0700

    chore, fix: add 2 experience & svg php

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.worldvectorlogo.com", "img.icons8.com", "www.svgrepo.com"],
+    domains: [
+      "cdn.worldvectorlogo.com",
+      "img.icons8.com",
+      "www.svgrepo.com",
+      "www.php.net",
+    ],
   },
 };
